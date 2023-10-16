8468de6e3cab946cd9a6cd5e4b1c2bab9da0dd2b
079268429Petr <p-ormanzhi@edu.hse.ru>
2023-10-16 22:49:41 +0300

    #main - Добавил тесты добавления и проверки добавленных в БД данных

---
+++ auth-service/AuthService.Tests/DatabaseTests/DatabaseTests.cs
@@ -0,0 +1,69 @@
+﻿using AuthService.Repositories.Codes;
+using AuthService.Services.Codes;
+using Moq;
+using Xunit;
+
+namespace AuthService.Tests.DatabaseTests;
+
+public class DatabaseTests
+{
+    [Theory]
+    [InlineData("+79282334657")]
+    [InlineData("89212334157")]
+    [InlineData("example@mail.ru")]
+    [InlineData("example@bk.ru")]
+    [InlineData("example@index.ru")]
+    [InlineData("example@inbox.ru")]
+    public async void CreateCodeTest(string credential)
+    {
+        var codeRepositoryMock = new Mock<ICodeRepository>();
+        await AddCredentials(codeRepositoryMock, credential);
+        codeRepositoryMock.Verify(repo => repo.CreateAsync(credential, CancellationToken.None), Times.Once);
+    }
+
+    [Theory]
+    [InlineData("+79282334657")]
+    [InlineData("89212334157")]
+    [InlineData("example@mail.ru")]
+    [InlineData("example@bk.ru")]
+    [InlineData("example@index.ru")]
+    [InlineData("example@inbox.ru")]
+    public async Task VerifyExistingCredentialsTest(string credential)
+    {
+        var codeRepositoryMock = new Mock<ICodeRepository>();
+
+        await AddCredentials(codeRepositoryMock, credential);
+        await VerifyCredentials(codeRepositoryMock, credential);
+
+        codeRepositoryMock.Verify(repo => repo.TryCodeAsync(credential, 1111, CancellationToken.None), Times.Once);
+    }
+
+    [Theory]
+    [InlineData("+79282334657")]
+    [InlineData("89212334157")]
+    [InlineData("example@mail.ru")]
+    [InlineData("example@bk.ru")]
+    [InlineData("example@index.ru")]
+    [InlineData("example@inbox.ru")]
+    public async Task VerifyNotExistingCredentialsTest(string credential)
+    {
+        var codeRepositoryMock = new Mock<ICodeRepository>();
+
+        await AddCredentials(codeRepositoryMock, credential);
+        await VerifyCredentials(codeRepositoryMock, "credential not exists");
+
+        codeRepositoryMock.Verify(repo => repo.TryCodeAsync(credential, 1111, CancellationToken.None), Times.Never);
+    }
+
+    private static async Task AddCredentials(Mock<ICodeRepository> mock, string credential)
+    {
+        var codeService = new CodeService(mock.Object);
+        await codeService.CreateAsync(credential, CancellationToken.None);
+    }
+
+    private static async Task VerifyCredentials(Mock<ICodeRepository> mock, string credential)
+    {
+        var codeService = new CodeService(mock.Object);
+        await codeService.TryCodeAsync(credential, 1111, CancellationToken.None);
+    }
+}
